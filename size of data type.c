2e4895a6b39cd0ed7edd5fa91d3bd3ff98ca6abb
kajalkushwaha567 <107293834+kajalkushwaha567@users.noreply.github.com>
2023-04-29 21:45:06 +0530

    Add files via upload

---
+++ size of data type.c
@@ -0,0 +1,46 @@
+//program to find out the size and limits of data types
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main(void)
+{
+	printf("sizeof(char)=%u\n",sizeof(char));
+	printf("sizeof(short)=%u\n",sizeof(short));
+	printf("sizeof(int)=%u\n",sizeof(int));
+	printf("sizeof(long)=%u\n",sizeof(long));
+	printf("sizeof(float)=%u\n",sizeof(char));
+	printf("sizeof(double)=%u\n",sizeof(double));
+	printf("sizeof(long double)=%u\n",sizeof(long double));
+	
+	printf("SCHAR_MIN = %d\n",SCHAR_MIN);
+	printf("SCHAR_MAX = %d\n",SCHAR_MAX);
+	printf("UCHAR_MIN = %d\n",UCHAR_MAX);
+	
+	printf("SHRT_MIN = %d\n",SHRT_MIN);
+	printf("SHRT_MAX = %d\n",SHRT_MAX);
+	printf("USHRT_MAX = %u\n",SHRT_MAX);
+	
+	printf("INT_MIN = %d\n",INT_MIN);
+	printf("INT_MAX = %d\n",INT_MAX);
+	printf("UINT_MAX = %u\n",UINT_MAX);
+	
+	printf("LONG_MIN = %ld\n",LONG_MIN);
+	printf("LONG_MAX = %ld\n",LONG_MAX);
+	printf("LONG_MAX = %lu\n",ULONG_MAX);
+	
+	printf("FLT_MIN = %e\n",FLT_MIN);
+	printf("FLT_MAX = %e\n",FLT_MAX);
+	
+	printf("DBL_MIN = %e\n",DBL_MIN);
+	printf("DBL_MAX = %e\n",DBL_MAX);
+	
+	printf("LDBL_MIN = %e\n",LDBL_MIN);
+	printf("LDBL_MAX = %e\n",LDBL_MAX);
+	
+	// number of digits of precision
+	printf("FLT_dIG = %d\n",FLT_DIG);
+	printf("DBL_dIG = %d\n",DBL_DIG);
+	printf("LDBL_dIG = %d\n",LDBL_DIG);
+	
+    return 0;
+}
