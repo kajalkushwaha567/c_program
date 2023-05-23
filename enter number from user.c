0ab4e8bf4c3b663b1ada401b7f10ec0c8e3d6937
kajalkushwaha567 <107293834+kajalkushwaha567@users.noreply.github.com>
2023-05-23 07:54:54 +0530

    Add files via upload

---
+++ enter number from user.c
@@ -0,0 +1,102 @@
+/* WAP that will accept any number from user and display yes,if user enter 5,7,9,11,13,22,27,46,95,107,205 otherwise no.*/
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter any number:");
+	scanf("%d",&n);
+	
+	if(n==5)
+	{
+	 printf("yes");	
+	}
+	else
+	{
+		if(n==7)
+		{
+			printf("yes");
+		}
+		else
+		{
+			if(n==9)
+			{
+				printf("yes");
+			}
+			else
+			{
+				if(n==11)
+				{
+					printf("yes");
+				}
+			
+			else
+			{
+				
+				if(n==13)
+				{
+					printf("yes");
+				}
+			
+				else
+			{
+				if(n==22)
+				{
+					printf("yes");
+				}
+				else
+			{
+				if(n==27)
+				{
+					printf("yes");
+				}
+				else
+				{
+					if(n==46)
+					{
+						printf("yes");
+					}
+					else
+					{
+						if(n==95)
+						{
+							printf("yes");
+						}
+						else
+						{
+							if(n==107)
+							{
+								printf("yes");
+							}
+							else
+							{
+								if(n==205)
+								{
+									printf("yes");
+								}
+								else
+								{
+									printf("no");
+								}
+							}
+						}
+					}
+				}
+				
+								}	
+				
+			}
+		}
+		}
+	}
+}
+return 0;
+}
+               			
+			
+			
+		
+	
+		
+	
+	
+	
