5b1273408f68f00cd27b9df47b6f5fd3089d7ee3
kajalkushwaha567 <107293834+kajalkushwaha567@users.noreply.github.com>
2023-05-29 22:26:57 +0530

    Add files via upload

---
+++ factorial of 4 number.c
@@ -0,0 +1,10 @@
+/* write a c program in which accept any four integer number
+ and print the factorial of all entered number.*/
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("Enter any 4 number:");
+	scanf("%d%*c%d%*c%d%*c%d%*c",&a,&b,&c,&d);
+	
+}
