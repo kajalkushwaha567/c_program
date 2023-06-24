1e9759b6754d2d742e1aca9a2e6d4bf51ccd4e1f
kajalkushwaha567 <107293834+kajalkushwaha567@users.noreply.github.com>
2023-06-24 17:07:29 +0530

    Add files via upload

---
+++ jai shree ganesh using for loop.c
@@ -0,0 +1,20 @@
+/*
+Jai sri Ganesh
+Jai sri Ganesh
+Jai sri Ganesh
+Jai sri Ganesh
+Jai sri Ganesh
+*/
+#include<stdio.h>
+int main()
+{
+	int i;
+	/*printf("\nJai Sri Ganesh");
+	printf("\nJai Sri Ganesh");
+	printf("\nJai Sri Ganesh");
+	printf("\nJai Sri Ganesh");
+	printf("\nJai Sri Ganesh");*/
+	for(i=1;i<=5;i++)
+	printf("\nJai Sri Ganesh");
+	
+}
